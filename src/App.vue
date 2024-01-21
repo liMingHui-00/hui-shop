ea4204f3ffde14dbb76c7a533eb463c8325dd5da
liminghui <3209498857@qq.com>
2024-01-21 20:18:35 +0800

    初始化项目&环境变量的配置

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+console.log(import.meta.env.VITE_A);
+
+</script>
+
+<template>
+  <div>app</div>
+</template>
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+let obj = {
+  name:'lisi'
+  ,age:15
+}
+console.log(obj);
+
+  
+</script>
+
+<style scoped></style>
