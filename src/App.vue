725b999e6bf8f1d76480658568bdeb3e5f19443a
liminghui <3209498857@qq.com>
2024-01-23 15:17:49 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@
 </script>
 
 <template>
-  <h3>app</h3>
-  <hello></hello>
+ <router-view></router-view>
+ <router-link to="cart">购物车</router-link>
+ <router-link to="user">用户</router-link>
+ <router-link to="search">搜索框</router-link>
+ <router-link to="home">主页</router-link>
 </template>
 <style scoped lang="scss">
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [{
+    path: '/cart',
+    component:()=>import('@/views/Cart.vue')
+  },{
+    path: '/search',
+    component:()=>import('@/views/Search.vue')
+  },{
+    path: '/user',
+    component:()=>import('@/views/User.vue')
+  },{
+    path: '/home',
+    component:()=>import('@/views/Home.vue')
+  }]
 })
 
 export default router
