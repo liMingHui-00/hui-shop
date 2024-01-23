016115aa2b012060b49f8d14e9ec162b712582f3
liminghui <3209498857@qq.com>
2024-01-23 22:00:41 +0800

    解决图片加载不出来的问题

--- src/views/product/ProductList.vue
+++ src/views/product/ProductList.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
-    <div v-for="item in products" :key="item.id">
-    <div class="img-wrap">
-      <img :src="item.image_url">
-    </div>
-    <h2 class="name">{{ item.name }}</h2>
-    <h3 class="price">{{ item.price }}</h3>
+    <div v-for="item in productsWithCorrectImage" :key="item.id">
+      <div class="img-wrap">
+        <img :src="item.correctImageUrl">
+      </div>
+      <h2 class="name">{{ item.name }}</h2>
+      <h3 class="price">{{ item.price }}</h3>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+
 // 创建一个ref数组
 const products = ref<Product[]>([])
+
 // 加载数据
-fetch(import.meta.env.VITE_BASE_API+'/products')
-.then(res => res.json() as Promise<Page<Product>>)
-.then(data => products.value=data.data
-)
+fetch(import.meta.env.VITE_BASE_API + '/products')
+  .then(res => res.json() as Promise<Page<Product>>)
+  .then(data => products.value = data.data)
 
+// 计算属性处理图片地址
+const productsWithCorrectImage = computed(() => {
+  return products.value.map(item => {
+    return {
+      ...item,
+      correctImageUrl: item.image_url.replace(".avif", "")
+    };
+  });
+});
 </script>
-
-<style scoped>
-
-</style>
